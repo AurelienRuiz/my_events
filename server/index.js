976bb9b12d7be107d0e09846eae262b962e917b4
Aurélien <aurelien.ruiz-minano@epitech.eu>
2020-02-21 11:39:40 +0100

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const axios = require('axios');
 const app = express();
+const cors = require("cors");
 const config = require('./config')[app.get('env')];
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World");

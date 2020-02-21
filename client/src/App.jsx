f60aa992b0f4dda5217601e44fee0df50b95e442
Aurélien <aurelien.ruiz-minano@epitech.eu>
2020-02-21 11:36:57 +0100

    First call API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
 import React, { Component} from "react";
+import config from "./config.json";
+import axios from "axios";
+
 
 class App extends Component{
+
+  componentDidMount() {
+    axios.get(`${config.server}home`)
+    .then(response => {
+      console.log(response.data);
+    })
+  }
   render(){
     return(
       <div className="App">
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 const app = express();
 const config = require('./config')[app.get('env')];
 
@@ -7,6 +8,13 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/home", (req, res) => {
+    axios.get(`${config.api.eventful.host}events/search?app_key=${config.api.eventful.key}&location=Nantes,France`)
+    .then(response => {
+        res.json(response.data);
+    })
 });
 
 app.listen(config.port, _ => {
